6dc4b08f79ace748acc0470a61935e74980a31fb
TLX2020 <65894219+TLX2020@users.noreply.github.com>
2025-02-18 23:06:17 +0800

    更新 HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,19 +5,7 @@
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    <template>
-  <div class="hello">
-    <h1>Hello World</h1>
-    <div>
-      <iframe 
-        src="./zb.html" 
-        width="100%" 
-        height="600px" 
-        frameborder="0"
-      ></iframe>
-    </div>
-  </div>
-</template>
+    <iframe src="./static-content.html" width="100%" height="600px" frameborder="0"></iframe>
     </p>
     <h3>电影资源分享</h3>
     <ul>

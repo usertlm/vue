56e769a96efc025615f70885cd8c22bb89cf1f28
TLX2020 <65894219+TLX2020@users.noreply.github.com>
2025-02-19 17:52:46 +0800

    更新 HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,7 +18,6 @@
       width="100%" 
       height="600px" 
       frameborder="0"
-    
     <ul>
       <li><a href="https://www.dy2018.com/" target="_blank" rel="noopener">电影天堂</a></li>
       <li><a href="https://www.agedm.org/" target="_blank" rel="noopener">AGE动漫</a></li>

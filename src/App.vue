20ad6c3b62c848eb3e5d1dfaae6473da4304062c
TLX2020 <65894219+TLX2020@users.noreply.github.com>
2025-02-19 16:39:18 +0800

    更新 App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,23 @@
   <HelloWorld msg="👇👇👇"/>
 </template>
 
+<template>
+  <div id="app">
+    <SearchComponent />
+  </div>
+</template>
+
+<script>
+import SearchComponent from './components/SearchComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SearchComponent
+  }
+};
+</script>
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 

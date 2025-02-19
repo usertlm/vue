de1a55240ff87a1fb2a4ec447ede2b6d033b1d67
TLX2020 <65894219+TLX2020@users.noreply.github.com>
2025-02-19 16:47:44 +0800

    更新 App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import SearchComponent from './components/SearchComponent.vue' // 确保路径正确
+import SearchComponent from './components/search.vue' // 确保路径正确
 
 export default {
   name: 'App',

27c8d30255ff85ec758fa81feff2f375efa6bd42
TLX2020 <65894219+TLX2020@users.noreply.github.com>
2025-07-01 22:43:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,42 @@
   </div>
 </template>
 
+<template>
+  <div>
+    <svg 
+      class="icon-link" 
+      @click="goToWebsite"
+      width="24" 
+      height="24" 
+      viewBox="0 0 24 24"
+    >
+      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToWebsite() {
+      window.open('http://www.staggeringbeauty.com/', '_blank')
+    }
+  }
+}
+</script>
+
+<style>
+.icon-link {
+  cursor: pointer;
+  fill: #409eff;
+  transition: fill 0.3s;
+}
+
+.icon-link:hover {
+  fill: #66b1ff;
+}
+</style>
+
 <script>
 // 引入组件
 import HelloWorld from './components/HelloWorld.vue'

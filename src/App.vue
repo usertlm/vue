66a49331ee44f603064190a6b0b60594951a1bee
usertlm <2756694314@qq.com>
2026-04-06 23:58:43 +0800

    fix: 将 PriceTrendChart 移至 HelloWorld(ChatBox) 下方、SearchComponent 上方

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,8 @@
       <HlsPlayer src="https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8" />
 
       <HelloWorld msg="👇👇👇" />
-      <SearchComponent />
       <PriceTrendChart />
+      <SearchComponent />
 
       <svg class="icon-link" @click="goToWebsite" width="24" height="24" viewBox="0 0 24 24">
         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
